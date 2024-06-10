7305529046c62eb5186a3811d964a091d6f1668d
Gui Almeida <guilherme.ampp@gmail.com>
2024-06-09 17:00:30 -0700

    Restructured `algorithms` project with Gradle Wrapper build

---
+++ algorithms/src/main/java/com/patterns/TwoPointers.java
@@ -0,0 +1,12 @@
+package com.patterns;
+
+/**
+ * Use whenever there is a requirements to find two data elements in an array that satisfy a certain condition.
+ *
+ * The two pointers can be used to traverse the data structure in one or both directions -- depending on the problem.
+ */
+public class TwoPointers {
+
+
+
+}
---
+++ algorithms/src/test/java/com/patterns/TwoPointersTest.java
@@ -0,0 +1,12 @@
+package com.patterns;
+
+import org.junit.Test;
+
+public class TwoPointersTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
