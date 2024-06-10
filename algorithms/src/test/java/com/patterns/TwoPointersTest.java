e5b83664cac471cd9ebe4083c32bc7b9303caba7
Gui Almeida <guilherme.ampp@gmail.com>
2024-06-09 18:00:59 -0700

    Another simple two pointers - move all zeros to the end

--- algorithms/src/test/java/com/patterns/TwoPointersTest.java
+++ algorithms/src/test/java/com/patterns/TwoPointersTest.java
@@ -8,15 +8,19 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * Use whenever there is a requirements to find two data elements in an array that satisfy a certain condition.
- *
+ * <p>
  * The two pointers can be used to traverse the data structure in one or both directions -- depending on the problem.
+ * <p>
+ * The problem matches this pattern if all conditions are fulfilled:
+ * - Linear data structure
+ * - Process pairs
+ * - Dynamic pointer movement
  */
 public class TwoPointersTest {
 
     @Test
     public void testReverseArrayInPlace() {
         final int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
-        final String expected = "7654321";
 
         int pointerStart = 0;
         int pointerEnd = array.length - 1;
@@ -27,11 +31,36 @@ public class TwoPointersTest {
             array[pointerEnd--] = tmp;
         }
 
-        assertEquals(expected,
-                Arrays.stream(array)
-                        .mapToObj(String::valueOf)
-                        .reduce((i, j) -> String.format("%s%s", i, j))
-                        .orElse(""));
+        assertEquals("7654321", arrayToString(array));
+    }
+
+    @Test
+    public void testMoveAllZerosToTheEnd() {
+        final int[] sortedArray = new int[]{1, 2, 0, 0, 5, 0, 8, 1};
+        int pointerLeft = 0;
+        int pointerRight = 0;
+
+        while (pointerRight < sortedArray.length) {
+            if (sortedArray[pointerRight] != 0) {
+                // swap and move both pointers!
+                int tmp = sortedArray[pointerLeft];
+                sortedArray[pointerLeft++] = sortedArray[pointerRight];
+                sortedArray[pointerRight++] = tmp;
+            } else if (sortedArray[pointerRight] == 0) {
+                // move right pointer
+                pointerRight++;
+            }
+        }
+
+        System.out.printf("Result: %s%n", Arrays.toString(sortedArray));
+        assertEquals("12581000", arrayToString(sortedArray));
+    }
+
+    private static String arrayToString(int[] array) {
+        return Arrays.stream(array)
+                .mapToObj(String::valueOf)
+                .reduce((i, j) -> String.format("%s%s", i, j))
+                .orElse("");
     }
 
 }
